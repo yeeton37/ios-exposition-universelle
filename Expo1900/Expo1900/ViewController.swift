1427e319176b2668d70e078be514fbac8259d60a
MinsuJeon <chso294@naver.com>
2022-06-15 10:42:51 +0900

    configuration 사용

--- Expo1900/Expo1900/ViewController.swift
+++ Expo1900/Expo1900/ViewController.swift
@@ -12,10 +12,7 @@ class KoreaEntry: UITableViewController {
         super.viewDidLoad()
         
         tableView.delegate = self
-        tableView.dataSource = self
-        
-        let nibName = UINib(nibName: "CustomCell", bundle: nil)
-        tableView.register(nibName, forCellReuseIdentifier: "CustomCell")
+        tableView.dataSource = self 
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -24,11 +21,27 @@ class KoreaEntry: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath) as! CustomCell
-        cell.imageView?.image = UIImage(named: "bangjja")
-        cell.topTitle?.text = "하이"
-        cell.bottomDescription?.text = "오만방자한 글으투입니다"
-//        var content = cell.defaultContentConfiguration()
-//        content.secondaryText = "hi"
+        //        cell.image1?.image = UIImage(named: "bangjja")
+        //        cell.topTitle?.text = "하이"
+        //        cell.bottomDescription?.text = "오만방자한 글으투입니다"
+        ////        var content = cell.defaultContentConfiguration()
+        ////        content.secondaryText = "hi"
+        
+        var content = cell.defaultContentConfiguration()
+        
+        content.text = "하이"
+        content.textProperties.font = UIFont.systemFont(ofSize: 20) // 폰트 사이즈
+        content.image = UIImage(named: "bangjja")
+        content.imageProperties.maximumSize.height = 30 // 이미지 높이
+        content.imageProperties.maximumSize.width = 30
+        content.secondaryText = "오만방자한 글으투입니다"
+
+        
+        cell.contentConfiguration = content
+        
+        /* 셀 자체 커스텀은 cell에서 직접 접근 */
+        
+        
         
         return cell
     }
